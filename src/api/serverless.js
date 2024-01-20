88f481c608acdbc1e06ce5434dd24ecea997d6aa
Bluce <a1019521a@gmail.com>
2024-01-20 18:17:34 +0800

    create Deploy branch

--- src/api/serverless.js
+++ src/api/serverless.js
@@ -1,7 +1,7 @@
 // Require the framework and instantiate it
 "use strict";
 
-import Axios from 'axios';
+
 
 // Read the .env file.
 import * as dotenv from "dotenv";
@@ -9,6 +9,8 @@ dotenv.config();
 import cors from '@fastify/cors';
 
 import Fastify from "fastify";
+
+
 const fastify = Fastify({
   logger: true,
 });
@@ -48,59 +50,23 @@ fastify.setErrorHandler(function (error, request, reply) {
 })
 
 
+/* Register  your application as a normal plugin.*/
+fastify.register(import("../src/app.js"));
 
 
-// Declare a route
+/* Run the server!*/
+// fastify.listen({ port: 8080 }, function (err, address) {
+//   if (err) {
+//     fastify.log.error(err);
+//     process.exit(1);
+//   }
+//   //Server is now listening on ${address}
+// });
 
-fastify.get("/",async function(req,reply){
-  return { hello: 'world' }
-});
 
-fastify.get("/api/aqi", async function (request, reply) {
-  const aqiURL=`${process.env.URL}${process.env.API_KEY}`;
-  try{
-    let response  = await Axios.get(aqiURL);
-    //let response  = await fetch('https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=4684ccb2-ffa1-4d65-b57e-3de48d92ab1e');
-    //console.log("repdata=", response.data);
-    reply.send(response.data);
-  }catch(err){
-    fastify.log.error(err);
-    process.exit(1);
-   //api.js console.error(`Error:${err}`);
-  }
-  
-});
-
-// Run the server!
-fastify.listen({ port: 8080 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  //Server is now listening on ${address}
-});
-
-// Register your application as a normal plugin.
-fastify.register(import("../src/api.js"));
 
 export default async (req, res) => {
   await fastify.ready();
   fastify.server.emit('request', req, res);
 }
 
-
-// {
-//   "version":2,
-//   "builid":[
-//     {
-//       "src":"src/api.js",
-//       "use":"@vercel/node"
-//     }
-//   ],
-//   "route":[
-//     {
-//       "src":"(.*)",
-//       "dest":"src/api.js"
-//     }
-//   ]
-// }
