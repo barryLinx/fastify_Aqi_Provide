ad8006087334a9401d23c93147cd239b78c5500a
Bluce <a1019521a@gmail.com>
2024-01-21 14:24:05 +0800

    restart Build

--- src/app.js
+++ src/app.js
@@ -1,13 +1,71 @@
+// Require the framework and instantiate it
+"use strict";
+
+
+
+// Read the .env file.
+import * as dotenv from "dotenv";
+dotenv.config();
+import cors from '@fastify/cors';
+// Require the framework
+import Fastify from "fastify";
+
+
+const fastify = Fastify({
+  logger: true,
+});
+
+//const cors_Orgin = process.env.VanillaJS_PMAQI || 'http://localhost:8050';
+
+await fastify.register(cors, { 
+  // put your options here
+  //origin:[cors_Orgin]
+   origin:'*'
+});
+
+
+// rate-limit 流量限制
+await fastify.register(import('@fastify/rate-limit'), {
+  //global : false,          // default true
+  max: 3,                 // default 1000
+  timeWindow: '1 minute',// default 1000 * 60
+  //allowList:[cors_Orgin],
+  errorResponseBuilder: function (request, context) {
+    return {
+      code: 429,
+      error: 'Too Many Requests',
+      message: `I only allow ${context.max} requests per ${context.after} to this Website. Try again soon.`,
+      date: Date.now(),
+      expiresIn: context.ttl // milliseconds
+    }
+  }
+});
+
+fastify.setErrorHandler(function (error, request, reply) {
+  if (error.statusCode === 429) {
+    reply.code(429)
+    error.message = 'You hit the rate limit! Slow down please!'
+  }
+  reply.send(error)
+})
+
+
+/* Register  your application as a normal plugin.*/
+//fastify.register(import("../api/serverless.js"));
+// import routes from '../api/serverless.js';
+// app.register(routes);
+
+
 import Axios from 'axios';
 
-async function routes (app, options) {
+
 // Declare a route
 
-app.get("/",async function(req,reply){
+fastify.get("/",async function(req,reply){
   return { hello: 'world' }
 });
 
-app.get("/api/aqi", async function (request, reply) {
+fastify.get("/api/aqi", async function (request, reply) {
   const aqiURL=`${process.env.URL}${process.env.API_KEY}`;
   try{
     let response  = await Axios.get(aqiURL);
@@ -15,13 +73,29 @@ app.get("/api/aqi", async function (request, reply) {
     //console.log("repdata=", response.data);
     reply.send(response.data);
   }catch(err){
-    app.log.error(err);
+    fastify.log.error(err);
     process.exit(1);
    //api.js console.error(`Error:${err}`);
   }
   
 });
 
-}
 
-export default routes;
+
+
+
+/* Run the server!*/
+fastify.listen({ port: 8080 }, function (err, address) {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  //Server is now listening on ${address}
+});
+
+
+
+
+
+
+
